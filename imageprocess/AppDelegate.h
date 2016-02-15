daf578e96c0be5a347c6b2ad634887c4f0736fab
yuchen <ycchang1006@gmail.com>
2016-02-15 09:11:01 +0800

    Initial Commit

---
+++ imageprocess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imageprocess
+//
+//  Created by yuchen on 2/15/16.
+//  Copyright © 2016 commandp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ imageprocess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imageprocess
+//
+//  Created by yuchen on 2/15/16.
+//  Copyright © 2016 commandp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
